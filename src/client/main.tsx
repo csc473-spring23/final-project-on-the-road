10ddeb2e5a9c273649fc590330ad1173c0eb2395
Jerry Chen <jchen056@citymail.cuny.edu>
2023-05-08 11:32:16 -0400

    f

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -3,16 +3,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-// import { StateProvider } from "./Components/FavList/StateProvider";
-// import reducer,{ initialState} from "./Components/FavList/reducer";
+
 
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-    {/* <StateProvider initialState={initialState} reducer={reducer}> */}
+
       <App />
-    {/* </StateProvider> */}
+
     </BrowserRouter>
   </React.StrictMode>
 );
