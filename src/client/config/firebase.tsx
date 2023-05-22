dae7d8e2d19e61f76befa9192bed03a92e511e42
dyzhao1011 <mikey501zhao@gmail.com>
2023-05-21 22:46:21 -0400

    final final

--- src/client/config/firebase.tsx
+++ src/client/config/firebase.tsx
@@ -24,15 +24,14 @@ import { addUser } from "./firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// REPLACE THE FOLLOWING CONFIGURATIONS WITH YOUR FIREBASE INFORMATION (DO NOT DELETE QUOTATIONS)
 const firebaseConfig = {
-  apiKey: "YOUR API KEY",
-  authDomain: "YOUR QUTH DOMAIN",
-  projectId: "YOUR PROJECT ID",
-  storageBucket: "YOUR STORAGE BUCKET",
-  messagingSenderId: "YOUR MESSAGING SENDER ID",
-  appId: "YOUR APP ID",
-  measurementId: "YOUR MEASUREMENT ID",
+  apiKey: "AIzaSyAaiAuxOludtjvuJ2L0G2CqMzKSP7OHN6A",
+  authDomain: "onetheroad-c4afe.firebaseapp.com",
+  projectId: "onetheroad-c4afe",
+  storageBucket: "onetheroad-c4afe.appspot.com",
+  messagingSenderId: "388717518917",
+  appId: "1:388717518917:web:010fc59dd0cbfff7f7432c",
+  measurementId: "G-6GEPBSFHME",
 };
 
 // Initialize Firebase
